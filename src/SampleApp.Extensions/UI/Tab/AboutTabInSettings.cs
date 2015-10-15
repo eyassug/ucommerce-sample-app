78dcae4abb09c55795e7b19182b2e79028b2f654
Mads Due <Mads Due@localhost>
2015-10-15 14:52:28 +0200

    Merge

--- src/SampleApp.Extensions/UI/Tab/AboutTabInSettings.cs
+++ src/SampleApp.Extensions/UI/Tab/AboutTabInSettings.cs
@@ -1,8 +1,27 @@
-﻿using UCommerce.Presentation.Web.Controls;
+﻿using SampleApp.Extensions.Model;
+using UCommerce.Pipelines;
+using UCommerce.Pipelines.ViewBuilder;
 
-namespace SampleApp.Extensions.NewTab
+namespace SampleApp.Extensions.UI.Tab
 {
-	public class AboutTabInSettings 
+	public class AboutTabInSettings : IPipelineTask<IPipelineArgs<ViewBuilderRequest, ViewBuilderResponse>>
 	{
+		public PipelineExecutionResult Execute(IPipelineArgs<ViewBuilderRequest, ViewBuilderResponse> subject)
+		{
+			if (subject.Request.ViewId != "settingsstartpage_aspx") return PipelineExecutionResult.Success;
+
+			var viewSection = new ViewSection
+			{
+				View = "../Apps/SampleApp/About.ascx",
+				ResourceKey = "",
+				MultiLingual = false,
+				HasSaveButton = false,
+				HasDeleteButton = false
+			};
+
+			subject.Response.ViewSections.Add(viewSection);
+
+			return PipelineExecutionResult.Success;
+		}
 	}
 }
