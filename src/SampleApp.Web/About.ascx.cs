d4aa8ce17cb806ae886d1f321cf5183a99b0e0fa
Thomas Arvidsen <Thomas Arvidsen@localhost>
2015-10-15 12:47:35 +0200

    Added a new user control to display on the tab.

---
+++ src/SampleApp.Web/About.ascx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.UI;
+using SampleApp.Extensions.Api;
+using UCommerce.EntitiesV2;
+
+namespace SampleApp.Web
+{
+	public partial class AboutUserControl : UserControl, INamed
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			uCommerceVersion.Text = SampleApi.uCommerceVersion();
+
+			SchemaVersion.Text = SampleApi.SchemaVersion();
+		}
+
+		private string _name = "About";
+
+		string INamedReadOnly.Name 
+		{ 
+			get { return _name; }
+		}
+
+		string INamed.Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+	}
+}
