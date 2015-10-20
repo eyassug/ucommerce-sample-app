20926fb10e0945aba8e67ffce26428e8a1d79fa8
Mads Due <Mads Due@localhost>
2015-10-20 12:51:01 +0200

    Added Nuget and PSake.

---
+++ src/SampleApp.Extensions/UI/Tab/AboutTabInSettings.cs
@@ -0,0 +1,27 @@
+﻿using SampleApp.Extensions.Model;
+using UCommerce.Pipelines;
+using UCommerce.Pipelines.ViewBuilder;
+
+namespace SampleApp.Extensions.UI.Tab
+{
+	public class AboutTabInSettings : IPipelineTask<IPipelineArgs<ViewBuilderRequest, ViewBuilderResponse>>
+	{
+		public PipelineExecutionResult Execute(IPipelineArgs<ViewBuilderRequest, ViewBuilderResponse> subject)
+		{
+			if (subject.Request.ViewId != "settingsstartpage_aspx") return PipelineExecutionResult.Success;
+
+			var viewSection = new ViewSection
+			{
+				View = "../Apps/SampleApp/About.ascx",
+				ResourceKey = "",
+				MultiLingual = false,
+				HasSaveButton = false,
+				HasDeleteButton = false
+			};
+
+			subject.Response.ViewSections.Add(viewSection);
+
+			return PipelineExecutionResult.Success;
+		}
+	}
+}
---
+++ src/SampleApp.Extensions/UI/Tree/AboutNodeProvider.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UCommerce.Tree;
+using UCommerce.Tree.Impl;
+
+namespace SampleApp.Extensions.UI.Tree
+{
+	/// <summary>
+	/// A content provider is a child node provider for supported node types.
+	/// </summary>
+	public class AboutNodeProvider : ITreeContentProvider
+	{
+		private readonly string _nodeName;
+		private readonly IList<string> _supportTypes;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public AboutNodeProvider(string nodeName, IList<string> supportTypes)
+		{
+			_nodeName = nodeName;
+			_supportTypes = supportTypes;
+		}
+
+		/// <summary>
+		/// Given a node type and possibly an id, the provider returnes a list of child nodes.
+		/// </summary>
+		/// <param name="nodeType">The type of the node.</param>
+		/// <param name="id">The id of the node.</param>
+		/// <returns>A list of child nodes for the node.</returns>
+		public IList<ITreeNodeContent> GetChildren(string nodeType, string id)
+		{
+			return new List<ITreeNodeContent>
+			{
+				new TreeNodeContent("version", -1)
+				{
+					Name = _nodeName,
+					HasChildren = false,
+					Action = "/Apps/SampleApp/About.aspx",
+					Icon = "/Apps/SampleApp/Media/about.png"
+				}
+			};
+		}
+
+		/// <summary>
+		/// Returns true, if the provider supports a specific tree node type.
+		/// </summary>
+		/// <param name="nodeType">The type of the node.</param>
+		/// <returns>true, if the node type is supported by the provider.</returns>
+		public bool Supports(string nodeType)
+		{
+			return _supportTypes.Contains(nodeType);
+		}
+	}
+}
