77283de219cf7d1e844ed7bcc88bdbecdbcd748b
Mads Due <Mads Due@localhost>
2015-10-20 12:55:58 +0200

    Merge

--- src/SampleApp.Extensions/UI/Tab/AboutTabInSettings.cs
+++ src/SampleApp.Extensions/UI/Tab/AboutTabInSettings.cs
@@ -1,4 +1,5 @@
-﻿using SampleApp.Extensions.Model;
+﻿using System;
+using SampleApp.Extensions.Model;
 using UCommerce.Pipelines;
 using UCommerce.Pipelines.ViewBuilder;
 
@@ -6,9 +7,16 @@ namespace SampleApp.Extensions.UI.Tab
 {
 	public class AboutTabInSettings : IPipelineTask<IPipelineArgs<ViewBuilderRequest, ViewBuilderResponse>>
 	{
+		private readonly TabConfiguration _configuration;
+
+		public AboutTabInSettings(TabConfiguration tabConfiguration)
+		{
+			_configuration = tabConfiguration;
+		}
+
 		public PipelineExecutionResult Execute(IPipelineArgs<ViewBuilderRequest, ViewBuilderResponse> subject)
 		{
-			if (subject.Request.ViewId != "settingsstartpage_aspx") return PipelineExecutionResult.Success;
+			if (subject.Request.ViewId != "settingsstartpage_aspx" || !_configuration.ShowTab) return PipelineExecutionResult.Success;
 
 			var viewSection = new ViewSection
 			{
--- src/SampleApp.Web/About.ascx.cs
+++ src/SampleApp.Web/About.ascx.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Web.UI;
 using SampleApp.Extensions.Api;
+using SampleApp.Extensions.Model;
 using UCommerce.EntitiesV2;
+using UCommerce.Infrastructure;
 
 namespace SampleApp.Web
 {
 	public partial class AboutUserControl : UserControl, INamed
 	{
+		private readonly TabConfiguration _configuration;
+
+		public AboutUserControl(TabConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public AboutUserControl() : this(ObjectFactory.Instance.Resolve<TabConfiguration>())
+		{
+		}
+
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			
+			uCommerceVersionHeader.Visible = _configuration.ShowUCommerceVersion;
+			SchemaVersionHeader.Visible = _configuration.ShowShemaVersion;
+
 			uCommerceVersion.Text = SampleApi.uCommerceVersion();
 
 			SchemaVersion.Text = SampleApi.SchemaVersion();
