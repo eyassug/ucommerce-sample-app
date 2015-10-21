85a0975a8afaa94f63969e4f6542f8c8cf6d67d5
Mads Due <Mads Due@localhost>
2015-10-21 13:13:16 +0200

    Updated scripts to extract files from project instead of outputDir.

---
+++ src/SampleApp.Extensions/Model/ViewSection.cs
@@ -0,0 +1,13 @@
+﻿using UCommerce.EntitiesV2.UI;
+
+namespace SampleApp.Extensions.Model
+{
+	public class ViewSection : IViewSection
+	{
+		public string View { get; set; }
+		public string ResourceKey { get; set; }
+		public bool MultiLingual { get; set; }
+		public bool HasSaveButton { get; set; }
+		public bool HasDeleteButton { get; set; }
+	}
+}
---
+++ src/SampleApp.Extensions/UI/Tab/AboutTabInSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SampleApp.Extensions.Model;
+using UCommerce.Pipelines;
+using UCommerce.Pipelines.ViewBuilder;
+
+namespace SampleApp.Extensions.UI.Tab
+{
+	public class AboutTabInSettings : IPipelineTask<IPipelineArgs<ViewBuilderRequest, ViewBuilderResponse>>
+	{
+		private readonly TabConfiguration _configuration;
+
+		public AboutTabInSettings(TabConfiguration tabConfiguration)
+		{
+			_configuration = tabConfiguration;
+		}
+
+		public PipelineExecutionResult Execute(IPipelineArgs<ViewBuilderRequest, ViewBuilderResponse> subject)
+		{
+			if (subject.Request.ViewId != "settingsstartpage_aspx" || !_configuration.ShowTab) return PipelineExecutionResult.Success;
+
+			var viewSection = new ViewSection
+			{
+				View = "../Apps/SampleApp/About.ascx",
+				ResourceKey = "",
+				MultiLingual = false,
+				HasSaveButton = false,
+				HasDeleteButton = false
+			};
+
+			subject.Response.ViewSections.Add(viewSection);
+
+			return PipelineExecutionResult.Success;
+		}
+	}
+}
