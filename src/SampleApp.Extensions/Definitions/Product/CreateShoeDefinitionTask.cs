b4d2fc10340f6da3a8b80dc42922d0672d4ba86a
Mads Due <Mads Due@localhost>
2015-10-22 09:13:30 +0200

    Hook in the initialize component to create/merge a new shoe definition.

---
+++ src/SampleApp.Extensions/Definitions/Product/CreateShoeDefinitionTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UCommerce.EntitiesV2;
+using UCommerce.Extensions;
+using UCommerce.Pipelines;
+
+namespace SampleApp.Extensions.Definitions.Product
+{
+	public class CreateShoeDefinitionTask : IPipelineTask<ProductDefinition>
+	{
+
+		public PipelineExecutionResult Execute(ProductDefinition subject)
+		{
+			subject = new ProductDefinition()
+			{
+				Name = "Shoe",
+				CreatedOn = DateTime.Now,
+				CreatedBy = "Mads",
+				ModifiedOn = DateTime.Now,
+				ModifiedBy = "Mads",
+
+			};
+
+			var definitionsFields = CreateDefinitionFields();
+			definitionsFields.ForEach(subject.AddProductDefinitionField);
+			
+			return PipelineExecutionResult.Success;
+		}
+
+		private IList<ProductDefinitionField> CreateDefinitionFields()
+		{
+			return new List<ProductDefinitionField>
+			{
+				new ProductDefinitionField()
+				{
+					DataType = DataType.FirstOrDefault(x => x.DataTypeId == 6),
+					Name = "Awesomeness",
+					RenderInEditor = true,
+				},
+				new ProductDefinitionField()
+				{
+					DataType = DataType.FirstOrDefault(x => x.DataTypeId == 1),
+					Name = "Short story",
+					RenderInEditor = true,
+				}
+			};
+		}
+	}
+}
---
+++ src/SampleApp.Extensions/Pipelines/Definitions/AddDefinitionPipeline.cs
@@ -0,0 +1,13 @@
+﻿using UCommerce.EntitiesV2;
+using UCommerce.Infrastructure.Logging;
+using UCommerce.Pipelines;
+
+namespace SampleApp.Extensions.Pipelines.Definitions
+{
+	public class AddDefinitionPipeline : Pipeline<ProductDefinition>
+	{
+		public AddDefinitionPipeline(IPipelineTask<ProductDefinition>[] tasks, ILoggingService loggingService) : base(tasks, loggingService)
+		{
+		}
+	}
+}
