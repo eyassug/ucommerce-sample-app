7cba141cd826c88b996fd6732f17ebeba75e126c
Mads Due <Mads Due@localhost>
2015-10-22 13:20:50 +0200

    Add pipeline (addShoeDefinitionPipeline) to be execute in the initialize pipeline.

---
+++ src/SampleApp.Extensions/Pipelines/ProductDefinition/AddShoeDefinitionTask.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UCommerce.Pipelines;
+
+namespace SampleApp.Extensions.Pipelines.ProductDefinition
+{
+	/// <summary>
+	/// Save the new ProductDefintion 
+	/// if ProductDefinition.Name dosn't exist. 
+	/// </summary>
+	public class AddShoeDefinitionTask : IPipelineTask<UCommerce.EntitiesV2.ProductDefinition>
+	{ 
+		public PipelineExecutionResult Execute(UCommerce.EntitiesV2.ProductDefinition subject)
+		{
+			if (UCommerce.EntitiesV2.ProductDefinition.All().Any(x => x.Name == subject.Name)) return PipelineExecutionResult.Success;
+						
+			subject.Save();
+
+			return PipelineExecutionResult.Success;
+		}
+	}
+}
---
+++ src/SampleApp.Extensions/Pipelines/ProductDefinition/ModifyShoeDefinitionTask.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UCommerce.EntitiesV2;
+using UCommerce.EntitiesV2.Definitions;
+using UCommerce.Extensions;
+using UCommerce.Pipelines;
+
+namespace SampleApp.Extensions.Pipelines.ProductDefinition
+{
+	/// <summary>
+	/// Modifies the ProductDefinition with content like
+	/// Name, DefinitionFields and DefinitionFieldDescriptions. 
+	/// </summary>
+	public class ModifyShoeDefinitionTask : IPipelineTask<UCommerce.EntitiesV2.ProductDefinition>
+	{
+		public PipelineExecutionResult Execute(UCommerce.EntitiesV2.ProductDefinition subject)
+		{
+			subject.Name = "Shoe";
+			subject.CreatedOn = DateTime.Now;
+			subject.CreatedBy = "Mads";
+			subject.ModifiedOn = DateTime.Now;
+			subject.ModifiedBy = "Mads";
+
+			var definitionsFields = CreateDefinitionFields();
+			definitionsFields.ForEach(subject.AddProductDefinitionField);
+			
+			return PipelineExecutionResult.Success;
+		}
+
+		private IEnumerable<ProductDefinitionField> CreateDefinitionFields()
+		{
+			return new List<ProductDefinitionField>
+			{
+				CreateProductDefinitionField("Awesomeness", new BooleanDataTypeDefinition()),
+				CreateProductDefinitionField("ShortStory", new ShortTextDataTypeDefinition())
+			};
+		}
+
+		private ProductDefinitionField CreateProductDefinitionField(string name, DataTypeDefinition definition)
+		{
+			var productDefinitionField = new ProductDefinitionField()
+			{
+				DataType = DataType.FirstOrDefault(x => x.DefinitionName == definition.Name),
+				Name = name,
+				RenderInEditor = true,
+			};
+			
+			var descriptions = CreateDefinitionFieldsDescriptions(name);
+			descriptions.ForEach(productDefinitionField.AddProductDefinitionFieldDescription);
+
+			return productDefinitionField;
+		}
+
+		private IEnumerable<ProductDefinitionFieldDescription> CreateDefinitionFieldsDescriptions(string displayName)
+		{
+			return new Collection<ProductDefinitionFieldDescription>
+			{
+				new ProductDefinitionFieldDescription
+				{
+					DisplayName = string.Format("{0}-US", displayName), 
+					CultureCode = "en-US"
+				},new ProductDefinitionFieldDescription
+				{
+					DisplayName = string.Format("{0}-GB", displayName), 
+					CultureCode = "en-GB"
+				}
+			};
+		}
+	}
+}
