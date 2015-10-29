f95c06bae91df617b4becd0ed5395b1a05e91e53
Mads Due <Mads Due@localhost>
2015-10-29 10:19:55 +0100

    Add a client side and a server side button.

---
+++ src/SampleApp.Extensions/UI/Button/AddClientSideButtonToSettingsSearchTask.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Web.UI.WebControls;
+using UCommerce.Pipelines;
+using UCommerce.Presentation;
+using UCommerce.Presentation.UI;
+
+namespace SampleApp.Extensions.UI.Button
+{
+	/// <summary>
+	/// Adds a Client side button to the Settings search view. 
+	/// </summary>
+	public class AddClientSideButtonToSettingsSearchTask : IPipelineTask<SectionGroup>
+	{
+		public PipelineExecutionResult Execute(SectionGroup subject)
+		{
+			//Finds the right section by filtering on Name and OriginalName 
+			var section = subject.Sections.FirstOrDefault(s => s.Name == "Common" && s.OriginalName == "IndexFromScratch.ascx");
+
+			if(section == null) return PipelineExecutionResult.Success;
+
+			section.Menu.AddMenuButton(CreateClientSideButton());
+
+			return PipelineExecutionResult.Success;
+		}
+
+		public ImageButton CreateClientSideButton()
+		{
+			var clientSideButton = new ImageButton();
+			clientSideButton.ImageUrl = Resources.Images.Menu.Delete;
+			clientSideButton.CausesValidation = false;
+			clientSideButton.Attributes.Add("onclick", "if (confirm('Are you sure you want to this?')) { return true; } else return false;");
+			return clientSideButton;
+		}
+	}
+}
---
+++ src/SampleApp.Extensions/UI/Button/AddServerSideButtonToSettingsSearchTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using UCommerce.Infrastructure;
+using UCommerce.Pipelines;
+using UCommerce.Presentation;
+using UCommerce.Presentation.UI;
+using UCommerce.Search.Indexers;
+
+namespace SampleApp.Extensions.UI.Button
+{
+	/// <summary>
+	/// Adds a Server side button to the Settings search view. 
+	/// Which index everything from scratch. 
+	/// </summary>
+	public class AddServerSideButtonToSettingsSearchTask : IPipelineTask<SectionGroup>
+	{
+		public PipelineExecutionResult Execute(SectionGroup subject)
+		{
+			//Finds the right section by filtering on Name and OriginalName 
+			var section = subject.Sections.FirstOrDefault(s => s.Name == "Common" && s.OriginalName == "IndexFromScratch.ascx");
+
+			if (section == null) return PipelineExecutionResult.Success;
+
+			section.Menu.AddMenuButton(CreateServerSideButton());
+
+			return PipelineExecutionResult.Success;
+		}
+
+		public ImageButton CreateServerSideButton()
+		{
+			var serverSideButton = new ImageButton();
+			serverSideButton.ImageUrl = Resources.Images.Menu.Sort;
+			serverSideButton.CausesValidation = false;
+			serverSideButton.ToolTip = "Index everything from scratch";
+			serverSideButton.Attributes.Add("onclick", "if (confirm('Are you sure you want to index everything from scratch?')) { return true; } else return false;");
+			serverSideButton.Click += IndexEverythingFromSratchMethod;
+			return serverSideButton;
+		}
+
+		protected void IndexEverythingFromSratchMethod(object sender, EventArgs e)
+		{
+			ObjectFactory.Instance.Resolve<ScratchIndexer>().Index();
+		}
+	}
+}
