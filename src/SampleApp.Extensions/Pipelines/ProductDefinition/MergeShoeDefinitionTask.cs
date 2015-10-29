5a5fa78bd30cc90430d4dc391281faf43e98921f
Mads Due <Mads Due@localhost>
2015-10-29 10:19:55 +0100

    Add a client side and a server side button.

---
+++ src/SampleApp.Extensions/Pipelines/ProductDefinition/MergeShoeDefinitionTask.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using UCommerce.EntitiesV2;
+using UCommerce.Pipelines;
+
+namespace SampleApp.Extensions.Pipelines.ProductDefinition
+{
+	/// <summary>
+	/// Merge the new ProductDefinition with existing ProductDefinition 
+	/// if ProductDefinition.Name exist. 
+	/// </summary>
+	public class MergeShoeDefinitionTask : IPipelineTask<UCommerce.EntitiesV2.ProductDefinition>
+	{
+		private readonly IRepository<UCommerce.EntitiesV2.ProductDefinition> _productDefinitionRepository;
+
+		public MergeShoeDefinitionTask(IRepository<UCommerce.EntitiesV2.ProductDefinition> productDefinitionRepository)
+		{
+			_productDefinitionRepository = productDefinitionRepository;
+		}
+
+		public PipelineExecutionResult Execute(UCommerce.EntitiesV2.ProductDefinition subject)
+		{
+			var existingShoeDefinition = _productDefinitionRepository.Select().FirstOrDefault(x => x.Name == subject.Name);
+
+			if (existingShoeDefinition == null) return PipelineExecutionResult.Success;
+
+			MergeProductDefinitionFields(existingShoeDefinition, subject);
+
+			existingShoeDefinition.Save();
+
+			return PipelineExecutionResult.Success;
+		}
+
+		/// <summary>
+		/// It's responsible for merging the new productDefinitionFields into the existing ProductDefinition.
+		/// If the new ProductDefinitionfield isn't already present in the existing ProductDefinition.  
+		/// </summary>
+		/// <param name="existingShoeDefinition"></param>
+		/// <param name="newProductDefinition"></param>
+		private void MergeProductDefinitionFields(UCommerce.EntitiesV2.ProductDefinition existingShoeDefinition, UCommerce.EntitiesV2.ProductDefinition newProductDefinition)
+		{
+			foreach (var productDefinitionField in newProductDefinition.ProductDefinitionFields)
+			{
+				if (ProductDefinitionfieldExist(existingShoeDefinition, productDefinitionField)) continue;
+
+				EnsureFieldNameIsUnique(existingShoeDefinition, productDefinitionField);
+
+				existingShoeDefinition.AddProductDefinitionField(productDefinitionField);
+			}
+		}
+
+		/// <summary>
+		/// Updates the name on the new ProductDefinitionField if a existing productDefintionField has the same name. 
+		/// </summary>
+		/// <param name="existingShoeDefinition"></param>
+		/// <param name="productDefinitionField"></param>
+		private void EnsureFieldNameIsUnique(UCommerce.EntitiesV2.ProductDefinition existingShoeDefinition, ProductDefinitionField productDefinitionField)
+		{
+			if (existingShoeDefinition.ProductDefinitionFields.Any(x => x.Name == productDefinitionField.Name && !x.Deleted))
+			{
+				productDefinitionField.Name = string.Format("{0}_{1}", productDefinitionField.Name,
+					productDefinitionField.DataType.DefinitionName);
+			}
+		}
+
+		/// <summary>
+		/// True if there is a productDefintionField with same Name and DateType which isn't deleted.
+		/// </summary>
+		/// <param name="existingShoeDefinition"></param>
+		/// <param name="productDefinitionField"></param>
+		/// <returns></returns>
+		private static bool ProductDefinitionfieldExist(UCommerce.EntitiesV2.ProductDefinition existingShoeDefinition, ProductDefinitionField productDefinitionField)
+		{
+			return existingShoeDefinition.ProductDefinitionFields.Any(x => x.Name == productDefinitionField.Name && x.DataType.DataTypeId == productDefinitionField.DataType.DataTypeId && !x.Deleted);
+		}
+	}
+}
---
+++ src/SampleApp.Web/About.ascx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web.UI;
+using SampleApp.Extensions.Api;
+using SampleApp.Extensions.Model;
+using UCommerce.EntitiesV2;
+using UCommerce.Infrastructure;
+
+namespace SampleApp.Web
+{
+	/// <summary>
+	/// User control that controls what content to show on the tab
+	/// </summary>
+	/// <remarks>
+	/// Implements the INamed interface so uCommerce can get the title of tab.
+	///  </remarks>
+	public partial class AboutUserControl : UserControl, INamed
+	{
+		private readonly TabConfiguration _configuration;
+
+		public AboutUserControl(TabConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public AboutUserControl() : this(ObjectFactory.Instance.Resolve<TabConfiguration>())
+		{
+		}
+
+		/// <summary>
+		/// Uses the TabConfiguration to determine what to show and loads the uCommerce version and the SchemaVersion 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			uCommerceVersionHeader.Visible = _configuration.ShowUCommerceVersion;
+			SchemaVersionHeader.Visible = _configuration.ShowShemaVersion;
+
+			uCommerceVersion.Text = SampleApi.uCommerceVersion();
+
+			SchemaVersion.Text = SampleApi.SchemaVersion();
+		}
+
+		private string _name = "About, INamed inferface";
+
+		/// <summary>
+		/// Used the get the title of tab
+		/// </summary>
+		string INamedReadOnly.Name
+		{
+			get { return _name; }
+		}
+
+		/// <summary>
+		/// Used the get the title of tab
+		/// </summary>
+		string INamed.Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+	}
+}
