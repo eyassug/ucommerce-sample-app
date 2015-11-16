e758d69643eca4959aeb25d70e9cd7c113d59ef0
MSK <MSK@localhost>
2015-11-16 14:54:50 +0100

    Pass build configuration in instead of defaulting to Debug.

--- tools/Deploy/Rebuild.App.Solution.ps1
+++ tools/Deploy/Rebuild.App.Solution.ps1
@@ -1,7 +1,7 @@
 properties {
     $src = $srcDir
     $solution_file = $Solution_file
-    $configuration = 'Debug'
+    $configuration = $configuration
 }
 
 task Compile -description "Compiles the complete solution" { 
@@ -9,6 +9,7 @@ task Compile -description "Compiles the complete solution" {
 	
     # Set Visual Studio version explicitly so the proper build tasks can be found
     # Disable post build event to avoid deploying the solution as part of packaging
+    Write-Host "Compiling solution with "$configuration
     Exec { msbuild "$solution_file" /p:Configuration=$configuration /m /p:VisualStudioVersion=12.0 /p:WarningLevel=0 /verbosity:quiet /p:PostBuildEvent= }
     
     Pop-Location
