9d94bdb9fd31556da82c625dca94274a776cba76
Mads Due <Mads Due@localhost>
2016-01-15 21:36:49 +0100

    Include nuspec file when deploying to locale website.

---
+++ src/SampleApp.Extensions/Pipelines/ProductDefinition/ModifyShoeDefinitionTask.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using UCommerce.EntitiesV2;
+using UCommerce.EntitiesV2.Definitions;
+using UCommerce.Extensions;
+using UCommerce.Pipelines;
+
+namespace SampleApp.Extensions.Pipelines.ProductDefinition
+{
+	/// <summary>
+	/// Modifies the ProductDefinition with content like
+	/// Name, DefinitionFields and DefinitionFieldDescriptions. 
+	/// </summary>
+	public class ModifyShoeDefinitionTask : IPipelineTask<UCommerce.EntitiesV2.ProductDefinition>
+	{
+		private readonly IRepository<DataType> _dataTypeRepository;
+
+		public ModifyShoeDefinitionTask(IRepository<DataType> dataTypeRepository)
+		{
+			_dataTypeRepository = dataTypeRepository;
+		}
+
+		public PipelineExecutionResult Execute(UCommerce.EntitiesV2.ProductDefinition subject)
+		{
+			subject.Name = "Shoe";
+
+			var definitionsFields = CreateProductDefinitionFields();
+			definitionsFields.ForEach(subject.AddProductDefinitionField);
+			
+			return PipelineExecutionResult.Success;
+		}
+
+		private IEnumerable<ProductDefinitionField> CreateProductDefinitionFields()
+		{
+			return new List<ProductDefinitionField>
+			{
+				CreateProductDefinitionField("Awesomeness", new BooleanDataTypeDefinition()),
+				CreateProductDefinitionField("ShortStory", new ShortTextDataTypeDefinition())
+			};
+		}
+
+		private ProductDefinitionField CreateProductDefinitionField(string name, DataTypeDefinition definition)
+		{
+			var productDefinitionField = new ProductDefinitionField()
+			{
+				DataType = _dataTypeRepository.Select().FirstOrDefault(x => x.DefinitionName == definition.Name),
+				Name = name,
+				RenderInEditor = true,
+			};
+			
+			var descriptions = CreateDefinitionFieldsDescriptions(name);
+			descriptions.ForEach(productDefinitionField.AddProductDefinitionFieldDescription);
+
+			return productDefinitionField;
+		}
+
+		/// <summary>
+		/// Creates multilingual descriptions for the product definition field. 
+		/// </summary>
+		/// <param name="displayName"></param>
+		/// <returns></returns>
+		private IEnumerable<ProductDefinitionFieldDescription> CreateDefinitionFieldsDescriptions(string displayName)
+		{
+			return new Collection<ProductDefinitionFieldDescription>
+			{
+				new ProductDefinitionFieldDescription
+				{
+					DisplayName = string.Format("{0}-US", displayName), 
+					CultureCode = "en-US"
+				},new ProductDefinitionFieldDescription
+				{
+					DisplayName = string.Format("{0}-GB", displayName), 
+					CultureCode = "en-GB"
+				}
+			};
+		}
+	}
+}
---
+++ src/SampleApp.Extensions/UI/Tab/AboutTabInSettings.cs
@@ -0,0 +1,57 @@
+﻿using SampleApp.Extensions.Configuration;
+using UCommerce;
+using UCommerce.EntitiesV2;
+using UCommerce.Pipelines;
+using UCommerce.Presentation.UI;
+
+namespace SampleApp.Extensions.UI.Tab
+{
+	/// <summary>
+	/// A pipeline task that adds a new tab to the setting node in uCommerce
+	/// </summary>
+	/// <remarks>
+	/// Used the tabConfiguration to determine if the tab should be displayed. 
+	/// </remarks>
+	public class AboutTabInSettings : IPipelineTask<SectionGroup>
+	{
+		private readonly TabConfiguration _configuration;
+
+		public AboutTabInSettings(TabConfiguration tabConfiguration)
+		{
+			_configuration = tabConfiguration;
+		}
+
+
+		public PipelineExecutionResult Execute(SectionGroup sectionGroup)
+		{
+			//If the view is not the one that we want to hook into, then do nothing
+			if (!_configuration.ShowTab || sectionGroup.GetViewName() != Constants.UI.Pages.Roots.Settings) return PipelineExecutionResult.Success;
+
+			var section = BuildSection(sectionGroup);
+			sectionGroup.AddSection(section);
+
+			//Sets the new tab as default tab when the settings node is clicked
+			sectionGroup.ActiveTabId = sectionGroup.Sections.IndexOf(section);
+
+			return PipelineExecutionResult.Success;
+		}
+
+		private Section BuildSection(SectionGroup sectionGroup)
+		{
+			var section = new Section
+			{
+				Name = "About",
+				ID = sectionGroup.CreateUniqueControlId()
+			};
+
+			var control = sectionGroup.View.LoadControl("/Apps/SampleApp/About.ascx");
+
+			//Get the name of the control if it implements the INamed inferface
+			if (control is INamed)
+				section.Name = (control as INamed).Name;
+
+			section.AddControl(control);
+			return section;
+		}
+	}
+}
